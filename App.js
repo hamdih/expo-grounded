73d6119895f06c47951fb1bfc9fb23df6276cda4
hamdih <hamdi.hmimy@us.fujitsu.com>
2020-11-09 14:28:16 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+Amplify.configure(config)
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
